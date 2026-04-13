4ab8325aa8d0480aceeadc0fc70e2361f340b197
Adeniran Ádám Kristóf <90899862+Adeniran24@users.noreply.github.com>
2026-04-13 08:22:29 +0200

    Magic School bug fix

--- GameApi/Models/DND2014/MagicSchool.cs
+++ GameApi/Models/DND2014/MagicSchool.cs
@@ -1,7 +1,16 @@
+using System.Text.Json.Serialization;
+
 public class MagicSchool
 {
+    [JsonPropertyName("index")]
     public string Index { get; set; } = string.Empty;
+
+    [JsonPropertyName("name")]
     public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("desc")]
     public string Desc { get; set; } = string.Empty;
+
+    [JsonPropertyName("url")]
     public string Url { get; set; } = string.Empty;
 }
